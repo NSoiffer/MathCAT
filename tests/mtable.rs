189b53d30f6393416cc590b1d72af76e4a85c166
Neil Soiffer <soiffer@alum.mit.edu>
2021-08-03 15:06:54 -0700

    finish off ClearSpeak prefs for matrix-like notations

--- tests/mtable.rs
+++ tests/mtable.rs
@@ -372,10 +372,10 @@ mod mtable {
         </math>
                                     ";
         test("ClearSpeak", expr, "the 4 by 4 matrix; \
-              row 1; 0, column 2; 3, column 3; 4, column 4; 3; \
-              row 2; 2, column 2; 1, column 3; 0, column 4; 9; \
-              row 3; 3, column 2; 0, column 3; 2, column 4; 1; \
-              row 4; 6, column 2; 2, column 3; 9, column 4; 0;");
+              row 1; column 1; 0, column 2; 3, column 3; 4, column 4; 3; \
+              row 2; column 1; 2, column 2; 1, column 3; 0, column 4; 9; \
+              row 3; column 1; 3, column 2; 0, column 3; 2, column 4; 1; \
+              row 4; column 1; 6, column 2; 2, column 3; 9, column 4; 0;");
     }
 
     #[test]
@@ -423,30 +423,453 @@ mod mtable {
        </math>
           ";
         test("ClearSpeak", expr, "the 4 by 2 matrix; \
-                  row 1; 1, column 2; 3; \
-                  row 2; 4, column 2; 2; \
-                  row 3; 2, column 2; 1; \
-                  row 4; 0, column 2; 5;\
+                  row 1; column 1; 1, column 2; 3; \
+                  row 2; column 1; 4, column 2; 2; \
+                  row 3; column 1; 2, column 2; 1; \
+                  row 4; column 1; 0, column 2; 5;\
         ");
     }
 
   // put absolute value test here since it is related to determinate and is small for its own file
   #[test]
-      fn simple_absolute_value() {
-        let expr = "<math>
-            <mrow><mrow><mo>|</mo> <mi>x</mi> <mo>|</mo></mrow></mrow>
-          </math>";
-          test("ClearSpeak", expr, "the absolute value of x");
-      }
+  fn simple_absolute_value() {
+    let expr = "<math>
+        <mrow><mrow><mo>|</mo> <mi>x</mi> <mo>|</mo></mrow></mrow>
+      </math>";
+      test("ClearSpeak", expr, "the absolute value of x");
+  }
       
   #[test]
-      fn absolute_value_plus_1() {
-        let expr = "<math>
-            <mrow><mrow><mo>|</mo>
-              <mrow><mi>x</mi><mo>+</mo><mn>1</mn> </mrow>
-            <mo>|</mo></mrow></mrow>
-          </math>";
-          test("ClearSpeak", expr, "the absolute value of x plus 1");
-      }
+  fn absolute_value_plus_1() {
+    let expr = "<math>
+        <mrow><mrow><mo>|</mo>
+          <mrow><mi>x</mi><mo>+</mo><mn>1</mn> </mrow>
+        <mo>|</mo></mrow></mrow>
+      </math>";
+      test("ClearSpeak", expr, "the absolute value of x plus 1");
+  }
     
-    }
+
+    // Test preferences
+  #[test]
+  fn simple_matrix_speak_col_num() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd> <mn>2</mn> </mtd>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>7</mn> </mtd>
+            <mtd><mn>5</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+      test_ClearSpeak("ClearSpeak_Matrix", "SpeakColNum",
+            expr, "the 2 by 2 matrix; row 1; column 1; 2, column 2; 1; row 2; column 1; 7, column 2; 5;");
+  }
+
+#[test]
+  fn col_matrix_3x1_speak_col_num() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>2</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>3</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "SpeakColNum",
+            expr, "the 3 by 1 column matrix; row 1; 1; row 2; 2; row 3; 3;");
+  }
+
+  #[test]
+  fn row_matrix_1x2_speak_col_num() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>[</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd> <mtd><mn>2</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>]</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "SpeakColNum",
+            expr, "the 1 by 2 row matrix; column 1; 1, column 2; 2;");
+  }
+
+  #[test]
+  fn matrix_2x2_speak_col_num() {
+    let expr = "<math><mrow><mrow><mo>(</mo><mrow>
+        <mtable>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        </mtable>
+        </mrow><mo>)</mo></mrow></mrow></math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "SpeakColNum",
+            expr, "the 2 by 2 matrix; row 1; column 1; b sub 1 1, column 2; b sub 1 2; \
+                                                   row 2; column 1; b sub 2 1, column 2; b sub 2 2;");
+  }
+
+
+  #[test]
+  fn simple_matrix_silent_col_num() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd> <mn>2</mn> </mtd>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>7</mn> </mtd>
+            <mtd><mn>5</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+      test_ClearSpeak("ClearSpeak_Matrix", "SilentColNum",
+            expr, "the 2 by 2 matrix; row 1; 2, 1; row 2; 7, 5;");
+  }
+
+#[test]
+  fn col_matrix_3x1_silent_col_num() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>2</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>3</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "SilentColNum",
+            expr, "the 3 by 1 column matrix; 1; 2; 3;");
+  }
+
+  #[test]
+  fn row_matrix_1x2_silent_col_num() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>[</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd> <mtd><mn>2</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>]</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "SilentColNum",
+            expr, "the 1 by 2 row matrix; 1, 2;");
+  }
+
+  #[test]
+  fn matrix_2x2_silent_col_num() {
+    let expr = "<math><mrow><mrow><mo>(</mo><mrow>
+        <mtable>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        </mtable>
+        </mrow><mo>)</mo></mrow></mrow></math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "SilentColNum",
+            expr, "the 2 by 2 matrix; row 1; b sub 1 1, b sub 1 2; \
+                                                   row 2; b sub 2 1, b sub 2 2;");
+  }
+
+
+  #[test]
+  fn simple_matrix_end_matrix() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd> <mn>2</mn> </mtd>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>7</mn> </mtd>
+            <mtd><mn>5</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+      test_ClearSpeak("ClearSpeak_Matrix", "EndMatrix",
+            expr, "the 2 by 2 matrix; row 1; 2, 1; row 2; 7, 5; end matrix");
+  }
+
+#[test]
+  fn col_matrix_3x1_end_matrix() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>2</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>3</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "EndMatrix",
+            expr, "the 3 by 1 column matrix; 1; 2; 3; end matrix");
+  }
+
+  #[test]
+  fn row_matrix_1x2_end_matrix() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>[</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd> <mtd><mn>2</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>]</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "EndMatrix",
+            expr, "the 1 by 2 row matrix; 1, 2; end matrix");
+  }
+
+  #[test]
+  fn matrix_2x2_end_matrix() {
+    let expr = "<math><mrow><mrow><mo>(</mo><mrow>
+        <mtable>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        </mtable>
+        </mrow><mo>)</mo></mrow></mrow></math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "EndMatrix",
+            expr, "the 2 by 2 matrix; row 1; column 1; b sub 1 1, column 2; b sub 1 2; \
+                                                   row 2; column 1; b sub 2 1, column 2; b sub 2 2; end matrix");
+  }
+
+
+  #[test]
+  fn simple_matrix_vector() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd> <mn>2</mn> </mtd>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>7</mn> </mtd>
+            <mtd><mn>5</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+      test_ClearSpeak("ClearSpeak_Matrix", "Vector",
+            expr, "the 2 by 2 matrix; row 1; 2, 1; row 2; 7, 5;");
+  }
+
+#[test]
+  fn col_matrix_3x1_vector() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>2</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>3</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "Vector",
+            expr, "the 3 by 1 column vector; 1; 2; 3;");
+  }
+
+  #[test]
+  fn row_matrix_1x2_vector() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>[</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd> <mtd><mn>2</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>]</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "Vector",
+            expr, "the 1 by 2 row vector; 1, 2;");
+  }
+
+  #[test]
+  fn matrix_2x2_vector() {
+    let expr = "<math><mrow><mrow><mo>(</mo><mrow>
+        <mtable>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        </mtable>
+        </mrow><mo>)</mo></mrow></mrow></math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "Vector",
+            expr, "the 2 by 2 matrix; row 1; column 1; b sub 1 1, column 2; b sub 1 2; \
+                                                   row 2; column 1; b sub 2 1, column 2; b sub 2 2;");
+  }
+
+
+  #[test]
+  fn simple_matrix_end_vector() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd> <mn>2</mn> </mtd>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>7</mn> </mtd>
+            <mtd><mn>5</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+      test_ClearSpeak("ClearSpeak_Matrix", "EndVector",
+            expr, "the 2 by 2 matrix; row 1; 2, 1; row 2; 7, 5; end matrix");
+  }
+
+#[test]
+  fn col_matrix_3x1_end_vector() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>(</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>2</mn> </mtd>
+          </mtr>
+          <mtr>
+            <mtd><mn>3</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>)</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "EndVector",
+            expr, "the 3 by 1 column vector; 1; 2; 3; end vector");
+  }
+
+  #[test]
+  fn row_matrix_1x2_end_vector() {
+    let expr = "<math display='block' xmlns='http://www.w3.org/1998/Math/MathML'>
+      <mrow>
+        <mrow><mo>[</mo>
+        <mrow>
+          <mtable>
+          <mtr>
+            <mtd><mn>1</mn> </mtd> <mtd><mn>2</mn> </mtd>
+          </mtr>
+          </mtable></mrow>
+        <mo>]</mo></mrow></mrow>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "EndVector",
+            expr, "the 1 by 2 row vector; 1, 2; end vector");
+  }
+
+  #[test]
+  fn matrix_2x2_end_vector() {
+    let expr = "<math><mrow><mrow><mo>(</mo><mrow>
+        <mtable>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>1</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        <mtr>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>1</mn></mrow></msub></mrow></mtd>
+            <mtd><mrow><msub><mi>b</mi><mrow><mn>2</mn><mn>2</mn></mrow></msub></mrow></mtd>
+        </mtr>
+        </mtable>
+        </mrow><mo>)</mo></mrow></mrow></math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "EndVector",
+            expr, "the 2 by 2 matrix; row 1; column 1; b sub 1 1, column 2; b sub 1 2; \
+                                                   row 2; column 1; b sub 2 1, column 2; b sub 2 2; end matrix");
+  }
+
+
+
+  #[test]
+  fn matrix_binomial() {
+    let expr = "<math>
+        <mo>(</mo><mrow>
+          <mtable><mtr><mtd><mn>3</mn></mtd></mtr><mtr><mtd><mn>2</mn></mtd></mtr></mtable>
+        </mrow><mo>)</mo>
+      </math>";
+    test_ClearSpeak("ClearSpeak_Matrix", "Combinatorics", expr, "3 choose 2");
+  }
+
+}
