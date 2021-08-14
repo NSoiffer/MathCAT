58b0f37a323119ac1324935cd6c2873509d7818a
Neil Soiffer <soiffer@alum.mit.edu>
2021-08-14 13:05:56 -0700

    reorganized test dir to support multiple language/styles better

---
+++ tests/en.rs
@@ -0,0 +1,17 @@
+#![allow(non_snake_case)]
+
+mod common;
+
+mod en {
+    mod ClearSpeak {
+        mod functions;
+        mod large_ops;
+        mod menclose;
+        mod mfrac;
+        mod mroot;
+        mod msup;
+        mod mtable;
+        mod sets;
+    }
+}
+
