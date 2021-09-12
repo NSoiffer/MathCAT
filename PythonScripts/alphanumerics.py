66f531e58c8febe7a2b35ab5ee497b3437ee037d
Neil Soiffer <soiffer@alum.mit.edu>
2021-09-12 12:32:15 -0700

    code to convert SRE files to generate more Unicode chars

--- PythonScripts/alphanumerics.py
+++ PythonScripts/alphanumerics.py
@@ -1,5 +1,4 @@
 # Nemeth alphanumeric unicode converter
-
 from bs4 import BeautifulSoup
 
 def create_unicode_from_html(in_file: str, out_file):
@@ -17,9 +16,24 @@ def generate_char(out_stream, hex: str, description: str, braille: str):
     out_stream.write('{:32}# 0x{} ({})\n'.format(
             first_part, hex, description))
 
+def generate_digits(out_file, prefix: str, hex: str, description: str):
+    with open(out_file, 'a', encoding='utf8') as out_stream:
+        i = 0
+        for digit in "⠴⠂⠆⠒⠲⠢⠖⠶⠦⠔":
+            as_int = int(hex,16) +i
+            generate_char(out_stream, "{0:x}".format(as_int), "{} {}".format('bold', i), prefix+'⠼'+digit)
+            i += 1
+
+
 import os
-if os.path.exists("alphanumerics.txt"):
-  os.remove("alphanumerics.txt")
-path = "C:/Dev/speech-rule-engine/sre-tests/output/nemeth/symbols/"
-for filename in os.listdir(path):
-   create_unicode_from_html(path+filename, "alphanumerics.txt")
+# if os.path.exists("alphanumerics.txt"):
+#   os.remove("alphanumerics.txt")
+# path = "C:/Dev/speech-rule-engine/sre-tests/output/nemeth/symbols/"
+# for filename in os.listdir(path):
+#    create_unicode_from_html(path+filename, "alphanumerics.txt")
+if os.path.exists("out"):
+  os.remove("out")
+
+generate_digits("out", "⠸", "1D7CE", "bold")
+generate_digits("out", "⠈", "1D7D8", "double struck (as script)")
+generate_digits("out", "⠠", "1D7E2", "sans-serif")
