528e9486b032bb6d2eec2737ae8a0742f7215385
NSoiffer <NSoiffer@users.noreply.github.com>
2022-06-03 23:00:12 +0100

    modify test so that env settings don't interfere

--- src/interface.rs
+++ src/interface.rs
@@ -676,6 +676,9 @@ mod tests {
     }
     #[test]
     fn can_recover_from_invalid_set_rules_dir() {
+        use std::env;
+        // MathCAT will check the env var "MathCATRulesDir" as an override, so the following test might succeed if we don't override the env var
+        env::set_var("MathCATRulesDir", "MathCATRulesDir");
         assert!(set_rules_dir("someInvalidRulesDir".to_string()).is_err());
         assert!(set_rules_dir(super::super::abs_rules_dir_path()).is_ok());
         assert!(set_mathml("<math><mn>1</mn></math>".to_string()).is_ok());
