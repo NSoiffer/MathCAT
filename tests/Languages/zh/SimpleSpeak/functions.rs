e14b6b272130f3166f64ed4619e53abc471da3c1
HonJang <hjyanghj@gmail.com>
2023-10-21 12:52:14 +0800

    SimpleSpeak/functions.rs

--- tests/Languages/zh/SimpleSpeak/functions.rs
+++ tests/Languages/zh/SimpleSpeak/functions.rs
@@ -15,7 +15,7 @@ fn trig_names() {
     <mi>csc</mi><mi>&#x03D5;</mi><mo>+</mo>
     <mi>cot</mi><mi>&#x03C6;</mi>
     </mrow></math>";
-    test("en", "SimpleSpeak", expr, "sine of x plus cosine of y plus tangent of z plus secant of alpha, plus cosecant of phi, plus cotangent of phi");
+    test("zh", "SimpleSpeak", expr, "sine x 加 cosine y 加 tangent z 加 secant alpha, 加 cosecant phi, 加 cotangent phi");
 }
 
 #[test]
@@ -28,119 +28,119 @@ fn hyperbolic_trig_names() {
     <mi>csch</mi><mi>&#x03D5;</mi><mo>+</mo>
     <mi>coth</mi><mi>&#x03C6;</mi>
     </mrow></math>";
-    test("en", "SimpleSpeak", expr, "hyperbolic sine of x, plus \
-                                hyperbolic cosine of y, plus \
-                                hyperbolic tangent of z, plus \
-                                hyperbolic secant of alpha, plus \
-                                hyperbolic cosecant of phi, plus \
-                                hyperbolic cotangent of phi");
+    test("zh", "SimpleSpeak", expr, "hyperbolic sine x, 加 \
+                                hyperbolic cosine y, 加 \
+                                hyperbolic tangent z, 加 \
+                                hyperbolic secant alpha, 加 \
+                                hyperbolic cosecant phi, 加 \
+                                hyperbolic cotangent phi");
 }
 
 
 #[test]
 fn inverse_trig() {
     let expr = "<math><msup><mi>sin</mi><mrow><mo>-</mo><mn>1</mn></mrow></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "inverse sine of x");
+    test("zh", "SimpleSpeak", expr, "反 sine x");
 }
 
 #[test]
 fn trig_squared() {
     let expr = "<math><msup><mi>sin</mi><mn>2</mn></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "sine squared of x");
+    test("zh", "SimpleSpeak", expr, "sine 平方 x");
 }
 
 #[test]
 fn trig_cubed() {
     let expr = "<math><msup><mi>tan</mi><mn>3</mn></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "tangent cubed of x");
+    test("zh", "SimpleSpeak", expr, "tangent 立方 x");
 }
 
 #[test]
 fn trig_fourth() {
     let expr = "<math><msup><mi>sec</mi><mn>4</mn></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "the fourth power of, secant of x");
+    test("zh", "SimpleSpeak", expr, "secant 4 次方; x");
 }
 
 
 #[test]
 fn trig_power_other() {
     let expr = "<math><msup><mi>sinh</mi><mrow>><mi>n</mi><mo>-</mo><mn>1</mn></mrow></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "the n minus 1 power of, hyperbolic sine of x");
+    test("zh", "SimpleSpeak", expr, "hyperbolic sine n 減 1 次方; x");
 }
 
 #[test]
 fn simple_log() {
     let expr = "<math> <mrow>  <mi>log</mi><mi>x</mi></mrow> </math>";
-    test("en", "SimpleSpeak", expr, "log x");
+    test("zh", "SimpleSpeak", expr, "log x");
 }
 
 #[test]
 fn normal_log() {
     let expr = "<math><mrow><mi>log</mi><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "the log of, open paren x plus y, close paren");
+    test("zh", "SimpleSpeak", expr, "log, 左小括 x 加 y 右小括");
 }
 
 #[test]
 fn simple_log_with_base() {
     let expr = "<math> <mrow>  <msub><mi>log</mi><mi>b</mi></msub><mi>x</mi></mrow> </math>";
-    test("en", "SimpleSpeak", expr, "the log base b of x");
+    test("zh", "SimpleSpeak", expr, "log 底 b x");
 }
 
 #[test]
 fn normal_log_with_base() {
     let expr = "<math><mrow><msub><mi>log</mi><mi>b</mi></msub><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "the log base b of, open paren x plus y, close paren");
+    test("zh", "SimpleSpeak", expr, "log 底 b, 左小括 x 加 y 右小括");
 }
 
 #[test]
 fn simple_ln() {
     let expr = "<math> <mrow>  <mi>ln</mi><mi>x</mi></mrow> </math>";
-    test("en", "SimpleSpeak", expr, "natural log x");
+    test("zh", "SimpleSpeak", expr, "l n x");
 }
 
 #[test]
 fn normal_ln() {
     let expr = "<math><mrow><mi>ln</mi><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "the natural log of, open paren x plus y, close paren");
+    test("zh", "SimpleSpeak", expr, "l n, 左小括 x 加 y 右小括");
 }
 
 #[test]
 fn normal_ln_terse() {
     let expr = "<math><mrow><mi>ln</mi><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test_prefs("en", "SimpleSpeak", vec![("Verbosity", "Terse")],
-                expr, "l n of, open x plus y close");
+    test_prefs("zh", "SimpleSpeak", vec![("Verbosity", "Terse")],
+                expr, "l n, 左小括 x 加 y 右小括");
 }
 
 #[test]
 fn simple_ln_terse() {
     let expr = "<math> <mrow>  <mi>ln</mi><mi>x</mi></mrow> </math>";
-    test_prefs("en", "SimpleSpeak", vec![("Verbosity", "Terse")],
+    test_prefs("zh", "SimpleSpeak", vec![("Verbosity", "Terse")],
                 expr, "l n x");
 }
 
 #[test]
 fn explicit_function_call_with_parens() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2061;</mo><mrow><mo>(</mo><mi>x</mi><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t of x");
+    test("zh", "SimpleSpeak", expr, "t x");
 }
 
 
 #[test]
 fn explicit_times_with_parens() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2062;</mo><mrow><mo>(</mo><mi>x</mi><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t times x");
+    test("zh", "SimpleSpeak", expr, "t 成 x");
 }
 
 #[test]
 fn explicit_function_call() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2061;</mo><mrow><mi>x</mi></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t of x");
+    test("zh", "SimpleSpeak", expr, "t x");
 }
 
 #[test]
 fn explicit_times() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2062;</mo><mrow><mi>x</mi></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t x");
+    test("zh", "SimpleSpeak", expr, "t x");
 }
 
 
@@ -150,7 +150,7 @@ fn explicit_times() {
 #[test]
 fn no_times_binomial() {
     let expr = "<math><mrow><mi>x</mi> <mo>&#x2062;</mo> <mi>y</mi></mrow></math>";
-    test("en", "SimpleSpeak", expr, "x y");
+    test("zh", "SimpleSpeak", expr, "x y");
 }
 
 #[test]
@@ -159,7 +159,7 @@ fn times_following_paren() {
         <mn>2</mn>
         <mrow>  <mo>(</mo> <mn>3</mn>  <mo>)</mo> </mrow>
         </mrow></math>";
-    test("en", "SimpleSpeak", expr, "2 times 3");
+    test("zh", "SimpleSpeak", expr, "2 成 3");
 }
 
 #[test]
@@ -168,7 +168,7 @@ fn times_preceding_paren() {
         <mrow>  <mo>(</mo> <mn>2</mn>  <mo>)</mo> </mrow>
         <mn>3</mn>
         </mrow></math>";
-    test("en", "SimpleSpeak", expr, "2 times 3");
+    test("zh", "SimpleSpeak", expr, "2 成 3");
 }
 
 #[test]
@@ -179,8 +179,8 @@ fn no_times_sqrt() {
         <mo>=</mo>
         <msqrt> <mrow>  <mi>a</mi><mi>b</mi></mrow> </msqrt>
         </mrow></math>";
-    test("en", "SimpleSpeak", expr, 
-            "the square root of eigh; the square root of b; is equal to, the square root of eigh b end root,");
+    test("zh", "SimpleSpeak", expr, 
+            "根號 a, 根號 b; 等於, 根號 a b 結束根號;");
 }
 
 /*
@@ -194,7 +194,7 @@ fn no_times_sqrt() {
         <mo>)</mo></mrow>
         <mi>x</mi>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "25 times x");
+        test("zh", "SimpleSpeak", expr, "25 成 x");
     }
 
     #[test]
@@ -205,7 +205,7 @@ fn no_times_sqrt() {
         <mrow><mi>x</mi><mi>y</mi></mrow>
         <mo>)</mo></mrow>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "b x y");
+        test("zh", "SimpleSpeak", expr, "b x y");
     }
 
     #[test]
@@ -216,7 +216,7 @@ fn no_times_sqrt() {
         <mrow><mo>&#x2212;</mo><mn>2</mn></mrow>
         <mo>)</mo></mrow>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "2 plus negative 2");
+        test("zh", "SimpleSpeak", expr, "2 加 負 2");
     }
 
 
@@ -228,7 +228,7 @@ fn no_times_sqrt() {
         <mo>)</mo></mrow>
         <mo>+</mo><mn>1</mn>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "negative 2 x, plus 1");
+        test("zh", "SimpleSpeak", expr, "負 2 x 加 1");
     }
 
     #[test]
@@ -244,7 +244,7 @@ fn no_times_sqrt() {
         </msup>
         </mrow>
     </mrow></math>";
-        test("en", "SimpleSpeak", expr, "open paren 2 x close paren squared");
+        test("zh", "SimpleSpeak", expr, "左小括 2 x 右小括 平方");
     }
 
     #[test]
@@ -257,7 +257,7 @@ fn no_times_sqrt() {
             <mfrac> <mn>1</mn><mn>2</mn></mfrac>
             <mo>)</mo></mrow></mrow>
     </mrow></math>";
-        test("en", "SimpleSpeak", expr, "2 plus 1 half");
+        test("zh", "SimpleSpeak", expr, "2 加 2 分之 1");
     }
 
 
@@ -269,7 +269,8 @@ fn no_times_sqrt() {
         <mrow> <mo arg='open'>(</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>)</mo>
         <mo>)</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr, "the open interval from c to d");
+    ///// mysterious intent 
+    test("zh", "SimpleSpeak",expr, "open interval 從 c 到 d");
 }
 
 #[test]
@@ -279,7 +280,7 @@ fn no_times_sqrt() {
             <mrow> <mo arg='open'>[(]</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>)</mo>
             <mo>)</mo></mrow>
         </math>";
-    test("en", "SimpleSpeak",expr, "the closed open interval from c to d");
+    test("zh", "SimpleSpeak",expr, "closed open interval 從 c 到 d");
 }
 
 
@@ -290,7 +291,7 @@ fn parens_interval_open_closed() {
         <mrow> <mo arg='open'>(</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>]</mo>
         <mo>]</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr,"the open closed interval from c to d");
+    test("zh", "SimpleSpeak",expr,"open closed interval 從 c 到 d");
 }
 
 
@@ -301,7 +302,7 @@ fn parens_interval_closed_closed() {
             <mrow> <mo arg='open'>[(]</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>]</mo>
             <mo>]</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr, "the closed interval from c to d");
+    test("zh", "SimpleSpeak",expr, "closed interval 從 c 到 d");
 }
 
     #[test]
@@ -311,8 +312,8 @@ fn parens_interval_closed_closed() {
         <mrow><mrow arg='start'><mo>-</mo> <mi>∞</mi></mrow><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>)</mo>
         <mo>)</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr,
-    "the open interval from negative infinity to d");
+    test("zh", "SimpleSpeak",expr,
+    "open interval 從 負 無限大 到 d");
 }
 
     #[test]
@@ -322,7 +323,7 @@ fn parens_interval_closed_closed() {
         <mrow><mrow arg='start'><mo>-</mo> <mi>∞</mi></mrow><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>]</mo>
         <mo>]</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr,
-    "the open closed interval from negative infinity to d");
+    test("zh", "SimpleSpeak",expr,
+    "open closed interval 從 負 無限大 到 d");
 }
 
