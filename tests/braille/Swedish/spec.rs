14c05b6c00e1039571e688402eccad3868255247
NSoiffer <NSoiffer@users.noreply.github.com>
2024-01-12 04:59:38 +0000

    Added rule for `mroot`.
    Added `mroot` test along with another test for fractions (grouping not needed)

--- tests/braille/Swedish/spec.rs
+++ tests/braille/Swedish/spec.rs
@@ -8,6 +8,12 @@ fn arith_5_4() {
 }
 
 #[test]
+fn fraction_6_1() {
+    let expr = r#"<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mi>x</mi><mn>2</mn></mfrac><mo>=</mo><mn>9</mn></math>"#;
+    test_braille("Swedish", expr, "⠭⠳⠼⠃⠀⠶⠀⠼⠊");
+}
+
+#[test]
 fn fraction_6_4() {
     let expr = "<math><mfrac><mrow><mi>x</mi><mo>+</mo><mn>1</mn></mrow><mrow><mi>x</mi><mo>-</mo><mn>1</mn></mrow></mfrac></math>";
     test_braille("Swedish", expr, "⠿⠦⠭⠲⠼⠁⠳⠭⠤⠼⠁⠿⠴");
@@ -37,4 +43,10 @@ fn script_7_12() {
 fn root_7_25() {
     let expr = "<math><msqrt><mn>25</mn></msqrt><mo>=</mo><mn>5</mn></math>";
     test_braille("Swedish", expr, "⠩⠼⠃⠑⠀⠶⠀⠼⠑");
+}
+
+#[test]
+fn root_7_32() {
+    let expr = "<math><mroot><mn>27</mn><mn>3</mn></mroot><mo>=</mo><mn>3</mn></math>";
+    test_braille("Swedish", expr, "⠬⠼⠉⠩⠼⠃⠛⠀⠶⠀⠼⠉");
 }
