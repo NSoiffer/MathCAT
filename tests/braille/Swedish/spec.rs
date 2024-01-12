5fd1dc322b9be320fa28c15aaf7c064bb53dfc05
NSoiffer <NSoiffer@users.noreply.github.com>
2024-01-12 05:24:47 +0000

    Handle a-j following a digit (modified SWEDISH_INDICATOR_REPLACEMENTS entry). Added test for that.

--- tests/braille/Swedish/spec.rs
+++ tests/braille/Swedish/spec.rs
@@ -1,6 +1,13 @@
 use crate::common::*;
 
 #[test]
+fn number_letters_1_1() {
+    // Note: test uses only one space to avoid triggering ommission code
+    let expr = "<math><mn>4</mn><mi>a</mi><mo>&#xA0;</mo><mn>5</mn><mi>B</mi><mo>&#xA0;</mo><mn>7</mn><mi>a</mi><mi>b</mi></math>";
+    test_braille("Swedish", expr, "⠼⠙⠱⠁⠀⠼⠑⠠⠃⠀⠼⠛⠱⠁⠃");
+}
+
+#[test]
 fn arith_5_4() {
     let expr = "<math><mi>y</mi><mo>=</mo><mn>5</mn><mo>+</mo><mi>x</mi></math>";
     test_braille_prefs("Swedish", vec![("UseSpacesAroundAllOperators", "true")], expr, "⠽⠀⠶⠀⠼⠑⠀⠲⠀⠭");
@@ -34,7 +41,6 @@ fn script_7_7() {
 #[test]
 fn script_7_12() {
     // From ChemType
-    init_logger();
     let expr = "<math><msubsup><mi>SO</mi><mn>4</mn><mrow><mn>2</mn><mo>-</mo></mrow></msubsup></math>";
     test_braille("Swedish", expr, "⠠⠠⠎⠕⠣⠼⠙⠘⠬⠼⠃⠤⠱");
 }
