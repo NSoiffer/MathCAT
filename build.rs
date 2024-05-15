8cc31ede5a0d75fa0c14ac430ca8c92045b77f02
NSoiffer <NSoiffer@users.noreply.github.com>
2024-05-15 07:12:23 +0100

    Now writes zip files with the correct paths for unzipping into the current dir
    Cleaned up the code some

--- build.rs
+++ build.rs
@@ -2,39 +2,44 @@
 //! rules.zip are needed so there is a way to get the rules dir into the build since your can't get from the crate.
 //! The expectation is that most builds (with the exception of WASM builds) will need a build.rs file to extract the rules.
 
-use std::fs::{read_dir, DirEntry, File};
+use std::fs::{read_dir, File};
 use std::io::{self, Read, Seek, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 // use zip::result::ZipResult;
-use zip::write::FileOptions;
+use zip::write::{ZipWriter, SimpleFileOptions};
 use zip::CompressionMethod;
-use zip::ZipWriter;
 
-fn zip_dir(parent_dir: &Path, options: FileOptions) -> Result<(), std::io::Error> {
+fn zip_dir(parent_dir: &Path, options: SimpleFileOptions) -> Result<(), std::io::Error> {
     for entry in read_dir(parent_dir)? {
         let entry = entry?;
         let entry_path = entry.path();
+
+        // .zip files return true for is_dir()
         if let Some(suffix) = entry_path.extension() {
             if suffix == "zip" {
                 continue;
             }
         }
-        println!("trying dir entry {:?}", entry_path.to_str());
+        // println!("trying dir entry {:?}", entry_path.to_str());
         if entry_path.is_dir(){
             if let Some(dir_name) = entry_path.components().last() {
-                let zip_name = String::from(dir_name.as_os_str().to_string_lossy() + ".zip");
-                let archive_path = entry_path.clone().join(&zip_name).canonicalize()?;
-                println!("archive_path: '{}'", archive_path.to_string_lossy());
+                let dir_name = dir_name.as_os_str().to_str().unwrap();
+                if  std::env::set_current_dir(&entry_path).is_err() {
+                    println!("cargo::warning=couldn't change to directory '{}'", &entry_path.display());
+                    continue;
+                }
+                let zip_name = dir_name.to_string() + ".zip";
+                // println!("zip_name='{}')", &zip_name);
+                let archive_path = PathBuf::from(&zip_name);
+                // println!("archive_path: '{}'", archive_path.to_string_lossy());
                 let zip_file = match File::create(archive_path) {
                     Ok(file) => file,
                     Err(e) => panic!("build.rs couldn't create {:?}: {}", &zip_name, e),
                 };
-                println!("  created file");
                 let mut zip = ZipWriter::new(zip_file);
-                zip_entry(&mut zip, entry, options)?;
+                zip_entry(&mut zip, &PathBuf::from("."), options)?;
                 zip.finish()?;
-                println!("  after zip.finish()");
             }
         }
     }
@@ -42,36 +47,37 @@ fn zip_dir(parent_dir: &Path, options: FileOptions) -> Result<(), std::io::Error
 }
 
 #[allow(clippy::unused_io_amount)]
-fn zip_entry<T: Write + Seek>(
-    zip: &mut ZipWriter<T>,
-    entry: DirEntry,
-    options: FileOptions,
+fn zip_entry<W: Write + Seek>(
+    zip: &mut ZipWriter<W>,
+    path: &Path,
+    options: SimpleFileOptions,
 ) -> io::Result<()> {
-    let path = entry.path();
 
+    // println!("current working dir: {}", std::env::current_dir()?.display());
     if path.is_dir() {
         for entry in read_dir(path)? {
-            zip_entry(zip, entry?, options)?;
+            zip_entry(zip, &entry?.path(), options)?;
         }
     } else if let Some(suffix) = path.extension() {
         let suffix = suffix.to_ascii_lowercase();
         if suffix == "yaml" || suffix == "yml" {
-            let file_name = path.to_str().unwrap();
-            // println!("  adding '{}'", file_name);
-            zip.start_file(file_name, options)?;
+            // println!("  adding '{}'", path.to_str().unwrap());
+            zip.start_file_from_path(path, options)?;
 
             let mut file = File::open(&path)?;
             let mut buffer = Vec::new();
     
             file.read_to_end(&mut buffer)?;
             zip.write_all(&buffer)?;
-            // println!("  ..finished '{}'", file_name);
+            // println!("  ..finished '{}'", path.to_str().unwrap());
         }
     }
 
     Ok(())
 }
 
+/// Zip up each language and braille dir
+/// Note: regional variations (including zh-cn and zh-tw) are zipped together into one zip file
 fn main() {
     // This doesn't work because the build claims OUT_DIR is not defined(?)
     // let archive = PathBuf::from(concat!(env!("OUT_DIR"),"/rules.zip"));
@@ -80,28 +86,20 @@ fn main() {
     // let archive: PathBuf = [out_dir, std::ffi::OsString::from("rules.zip")].iter().collect();
     // eprintln!("zip file location: '{:?}'", archive.to_str());
 
-    // let archive = match File::create(&archive) {
-    //     Ok(file) => file,
-    //     Err(e) => panic!("build.rs couldn't create {:?}: {}", archive.to_str(), e),
-    // };
-    // let root_dir = std::env::var_os("CARGO_MANIFEST_DIR ").unwrap(); 
-    // let zip_directory: PathBuf = [root_dir.clone(), std::ffi::OsString::from("Rules")].iter().collect();
-    // eprintln!("rules dir: '{:?}'", zip_directory.to_str());
-
-    let zip_options = FileOptions::default().compression_method(CompressionMethod::Deflated)
+    let zip_options = SimpleFileOptions::default().compression_method(CompressionMethod::Deflated)
                     .compression_level(Some(9));
 
-    let rules_dir = std::env::current_exe().unwrap().parent().unwrap()
-                    .join("../../../../Rules/")
-                    .to_str().unwrap().to_string();
-    
-    println!("rules_dir = '{}'", rules_dir);
-    if let Err(e) = zip_dir(Path::new(&(rules_dir.clone() + "Languages")), zip_options.clone()) {
+    let rules_dir = match std::env::current_exe().unwrap().parent().unwrap()
+                            .join("../../../../Rules/")
+                            .canonicalize() {
+                        Ok(path) => path,
+                        Err(err) => panic!("Error canonicalizing path: {}", err),
+                    };
+    if let Err(e) = zip_dir(&rules_dir.join("Languages"), zip_options.clone()) {
         panic!("Error: {}", e);
     }
-    if let Err(e) = zip_dir(Path::new(&(rules_dir + "Braille")), zip_options) {
+    if let Err(e) = zip_dir(&rules_dir.join("Braille"), zip_options) {
         panic!("Error: {}", e);
     }
-    println!("cargo:rerun-if-changed=Rules");
-    panic!("Forced panic for debugging build.rs")
+    println!("cargo::rerun-if-changed=Rules");
 }
