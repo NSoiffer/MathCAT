d4e4a14abd5b4e318c4ff2727d273bf2ab7e0266
AndersEkl <39799609+AndersEkl@users.noreply.github.com>
2024-05-15 10:49:23 +0200

    Translated SimpleSpeak:mfrac.rs

--- tests/Languages/sv/SimpleSpeak/mfrac.rs
+++ tests/Languages/sv/SimpleSpeak/mfrac.rs
@@ -8,7 +8,7 @@ fn common_fraction_half() {
     let expr = "<math>
                     <mfrac> <mn>1</mn> <mn>2</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "1 half");
+    test("sv", "SimpleSpeak", expr, "en halv");
 }
 
 #[test]
@@ -16,7 +16,7 @@ fn common_fraction_thirds() {
     let expr = "<math>
                     <mfrac> <mn>2</mn> <mn>3</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "2 thirds");
+    test("sv", "SimpleSpeak", expr, "2 tredjedelar");
 }
 
 #[test]
@@ -24,7 +24,7 @@ fn common_fraction_tenths() {
     let expr = "<math>
                     <mfrac> <mn>17</mn> <mn>10</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "17 tenths");
+    test("sv", "SimpleSpeak", expr, "17 tiondelar");
 }
 
 #[test]
@@ -33,7 +33,7 @@ fn not_SimpleSpeak_common_fraction_tenths() {
     let expr = "<math>
                     <mfrac> <mn>89</mn> <mn>10</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "89 over 10,");
+    test("sv", "SimpleSpeak", expr, "89 genom 10,");
 }
 
 #[test]
@@ -49,7 +49,7 @@ fn non_simple_fraction() {
         </mrow>
     </math>
                             ";
-    test("sv", "SimpleSpeak", expr, "fraction, x plus y, over, x minus y, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, x plus y, genom, x minus y, slut division;");
 }
 
 #[test]
@@ -65,7 +65,7 @@ fn nested_fraction() {
         </mrow>
     </math>
                             ";
-    test("sv", "SimpleSpeak", expr, "fraction, x plus, fraction, 1 over y, end fraction; over, x minus y, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, x plus, division, 1 genom y, slut division; genom, x minus y, slut division;");
 }
 
 
@@ -82,7 +82,7 @@ fn deeply_nested_fraction_msqrt() {
         </mrow>
     </math>
                             ";
-    test("sv", "SimpleSpeak", expr, "fraction, x plus, the square root of 1 over y, end root; over, x minus y, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, x plus, kvadratroten ur 1 genom y, slut rot; genom, x minus y, slut division;");
 }
 
 #[test]
@@ -98,7 +98,7 @@ fn deeply_nested_fraction_mrow_msqrt() {
         </mrow>
     </math>
                             ";
-    test("sv", "SimpleSpeak", expr, "fraction, x plus, the square root of 2 plus 1 over y, end root; over, x minus y, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, x plus, kvadratroten ur 2 plus 1 genom y, slut rot; genom, x minus y, slut division;");
 }
 
 #[test]
@@ -114,7 +114,7 @@ fn numerator_simple_fraction() {
         </mrow>
     </math>
                             ";
-    test("sv", "SimpleSpeak", expr, "fraction, x over, x minus y, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, x genom, x minus y, slut division;");
 }
 
 #[test]
@@ -127,7 +127,7 @@ fn denominator_simple_fraction() {
         </mfrac>
     </math>
                             ";
-    test("sv", "SimpleSpeak", expr, "fraction, x minus y, over x, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, x minus y, genom x, slut division;");
 }
 
 
@@ -137,7 +137,7 @@ fn mixed_number() {
                     <mn>3</mn>
                     <mfrac> <mn>1</mn> <mn>2</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "3 and 1 half");
+    test("sv", "SimpleSpeak", expr, "3 och en halv");
 }
 
 #[test]
@@ -147,7 +147,7 @@ fn explicit_mixed_number() {
                     <mo>&#x2064;</mo>
                     <mfrac> <mn>1</mn> <mn>8</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "3 and 1 eighth");
+    test("sv", "SimpleSpeak", expr, "3 och en åttondel");
 }
 
 #[test]
@@ -156,7 +156,7 @@ fn mixed_number_big() {
                     <mn>3</mn>
                     <mfrac> <mn>7</mn> <mn>83</mn> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "3 and 7 eighty thirds");
+    test("sv", "SimpleSpeak", expr, "3 och 7 genom 83,");
 }
 
 #[test]
@@ -164,7 +164,7 @@ fn simple_text() {
     let expr = "<math>
     <mfrac> <mi>rise</mi> <mi>run</mi> </mfrac>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "rise over run,");
+    test("sv", "SimpleSpeak", expr, "rise genom run,");
 }
 
 #[test]
@@ -177,7 +177,7 @@ fn number_and_text() {
                 <mn>3</mn><mtext>gallons</mtext></mrow>
             </mfrac>
         </math>";
-    test("sv", "SimpleSpeak", expr, "fraction, 2 miles, over, 3 gallons, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, 2 miles, genom, 3 gallons, slut division;");
 }
 
 
@@ -201,7 +201,7 @@ fn nested_simple_fractions() {
                 </mfrac>
                 </mrow>
             </math>";
-    test("sv", "SimpleSpeak", expr, "fraction, 1 half, over, 2 thirds, end fraction;");
+    test("sv", "SimpleSpeak", expr, "division, en halv, genom, 2 tredjedelar, slut division;");
 }
 
 #[test]
@@ -212,5 +212,5 @@ fn binomial() {
                     <mfrac linethickness='0'> <mn>7</mn> <mn>3</mn> </mfrac>
                     <mo>)</mo>
                 </math>";
-    test("sv", "SimpleSpeak", expr, "2 times 7 choose 3");
+    test("sv", "SimpleSpeak", expr, "2 gånger, 7 över 3");
 }
