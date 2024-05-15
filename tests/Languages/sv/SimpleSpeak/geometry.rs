ed01916b6bc97fb49cbf1e8041d7271854b4177e
AndersEkl <39799609+AndersEkl@users.noreply.github.com>
2024-05-15 09:43:47 +0200

    Update geometry.rs

--- tests/Languages/sv/SimpleSpeak/geometry.rs
+++ tests/Languages/sv/SimpleSpeak/geometry.rs
@@ -5,23 +5,23 @@ use crate::common::*;
 #[test]
 fn arc() {
   let expr = "<math>  <mover><mrow><mi>B</mi><mi>C</mi></mrow><mo>⌒</mo></mover> </math>";
-  test("sv", "SimpleSpeak", expr, "arc cap b cap c");
+  test("sv", "SimpleSpeak", expr, "bågen versal b versal c");
 }
 
 #[test]
 fn ray() {
   let expr = "<math> <mover><mrow><mi>X</mi><mi>Y</mi></mrow><mo>&#xAF;</mo></mover> </math>";
-  test("sv", "SimpleSpeak", expr, "line segment cap x cap y");
+  test("sv", "SimpleSpeak", expr, "linje-segmentet versal x versal y");
 }
 
 #[test]
 fn arc_mtext() {
   let expr = "<math> <mover><mtext>BC</mtext><mo>⌒</mo></mover> </math>";
-  test("sv", "SimpleSpeak", expr, "arc cap b cap c");
+  test("sv", "SimpleSpeak", expr, "bågen versal b versal c");
 }
 
 #[test]
 fn ray_mtext() {
   let expr = "<math> <mover><mtext>XY</mtext><mo>→</mo></mover> </math>";
-  test("sv", "SimpleSpeak", expr, "ray cap x cap y");
+  test("sv", "SimpleSpeak", expr, "den riktade sträckan versal x versal y");
 }
