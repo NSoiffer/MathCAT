090604ba01a1d46821e4a8cf4037149e0cade601
samimaat <saminiksi@gmail.com>
2024-05-18 23:10:11 +0300

    translate

--- tests/Languages/fi/SimpleSpeak/functions.rs
+++ tests/Languages/fi/SimpleSpeak/functions.rs
@@ -15,7 +15,7 @@ fn trig_names() {
     <mi>csc</mi><mi>&#x03D5;</mi><mo>+</mo>
     <mi>cot</mi><mi>&#x03C6;</mi>
     </mrow></math>";
-    test("en", "SimpleSpeak", expr, "sine of x plus cosine of y plus tangent of z plus secant of alpha, plus cosecant of phi, plus cotangent of phi");
+    test("fi", "SimpleSpeak", expr, "sini arvolla x, plus kosini arvolla y, plus tangentti arvolla z, plus sekantti arvolla alfa, plus, kosekantti arvolla suora fii, plus kotangentti arvolla fii");
 }
 
 #[test]
@@ -28,119 +28,114 @@ fn hyperbolic_trig_names() {
     <mi>csch</mi><mi>&#x03D5;</mi><mo>+</mo>
     <mi>coth</mi><mi>&#x03C6;</mi>
     </mrow></math>";
-    test("en", "SimpleSpeak", expr, "hyperbolic sine of x, plus \
-                                hyperbolic cosine of y, plus \
-                                hyperbolic tangent of z, plus \
-                                hyperbolic secant of alpha, plus \
-                                hyperbolic cosecant of phi, plus \
-                                hyperbolic cotangent of phi");
+    test("fi", "SimpleSpeak", expr, "hyperbolinen sini arvolla x, plus, hyperbolinen kosini arvolla y, plus, hyperbolinen tangentti, arvolla z, plus, hyperbolinen sekantti, arvolla alfa; plus, hyperbolinen kosekantti, arvolla suora fii; plus, hyperbolinen kotangentti, arvolla fii");
 }
 
 
 #[test]
 fn inverse_trig() {
     let expr = "<math><msup><mi>sin</mi><mrow><mo>-</mo><mn>1</mn></mrow></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "inverse sine of x");
+    test("fi", "SimpleSpeak", expr, "käänteis sini arvolla x");
 }
 
 #[test]
 fn trig_squared() {
     let expr = "<math><msup><mi>sin</mi><mn>2</mn></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "sine squared of x");
+    test("fi", "SimpleSpeak", expr, "sini neliö arvolla x");
 }
 
 #[test]
 fn trig_cubed() {
     let expr = "<math><msup><mi>tan</mi><mn>3</mn></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "tangent cubed of x");
+    test("fi", "SimpleSpeak", expr, "tangentti kuutio arvolla x");
 }
 
 #[test]
 fn trig_fourth() {
     let expr = "<math><msup><mi>sec</mi><mn>4</mn></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "the fourth power of, secant of x");
+    test("fi", "SimpleSpeak", expr, "sekantti potenssiin 4, arvolla x");
 }
 
 
 #[test]
 fn trig_power_other() {
     let expr = "<math><msup><mi>sinh</mi><mrow>><mi>n</mi><mo>-</mo><mn>1</mn></mrow></msup><mi>x</mi></math>";
-    test("en", "SimpleSpeak", expr, "the n minus 1 power of, hyperbolic sine of x");
+    test("fi", "SimpleSpeak", expr, "hyperbolinen sini potenssiin n miinus 1; arvolla x");
 }
 
 #[test]
 fn simple_log() {
     let expr = "<math> <mrow>  <mi>log</mi><mi>x</mi></mrow> </math>";
-    test("en", "SimpleSpeak", expr, "log x");
+    test("fi", "SimpleSpeak", expr, "log x");
 }
 
 #[test]
 fn normal_log() {
     let expr = "<math><mrow><mi>log</mi><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "the log of, open paren x plus y, close paren");
+    test("fi", "SimpleSpeak", expr, "logaritmi, auki sulku x plus y, kiinni sulku");
 }
 
 #[test]
 fn simple_log_with_base() {
     let expr = "<math> <mrow>  <msub><mi>log</mi><mi>b</mi></msub><mi>x</mi></mrow> </math>";
-    test("en", "SimpleSpeak", expr, "the log base b of x");
+    test("fi", "SimpleSpeak", expr, "log kanta b arvolla x");
 }
 
 #[test]
 fn normal_log_with_base() {
     let expr = "<math><mrow><msub><mi>log</mi><mi>b</mi></msub><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "the log base b of, open paren x plus y, close paren");
+    test("fi", "SimpleSpeak", expr, "log kanta b arvolla, auki sulku x plus y, kiinni sulku");
 }
 
 #[test]
 fn simple_ln() {
     let expr = "<math> <mrow>  <mi>ln</mi><mi>x</mi></mrow> </math>";
-    test("en", "SimpleSpeak", expr, "natural log x");
+    test("fi", "SimpleSpeak", expr, "luonnollinen log x");
 }
 
 #[test]
 fn normal_ln() {
     let expr = "<math><mrow><mi>ln</mi><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "the natural log of, open paren x plus y, close paren");
+    test("fi", "SimpleSpeak", expr, "luonnollinen logaritmi, auki sulku x plus y, kiinni sulku");
 }
 
 #[test]
 fn normal_ln_terse() {
     let expr = "<math><mrow><mi>ln</mi><mrow><mo>(</mo><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow><mo>)</mo></mrow></mrow></math>";
-    test_prefs("en", "SimpleSpeak", vec![("Verbosity", "Terse")],
-                expr, "l n of, open x plus y close");
+    test_prefs("fi", "SimpleSpeak", vec![("Verbosity", "Terse")],
+                expr, "l n, auki x plus y kiinni");
 }
 
 #[test]
 fn simple_ln_terse() {
     let expr = "<math> <mrow>  <mi>ln</mi><mi>x</mi></mrow> </math>";
-    test_prefs("en", "SimpleSpeak", vec![("Verbosity", "Terse")],
+    test_prefs("fi", "SimpleSpeak", vec![("Verbosity", "Terse")],
                 expr, "l n x");
 }
 
 #[test]
 fn explicit_function_call_with_parens() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2061;</mo><mrow><mo>(</mo><mi>x</mi><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t of x");
+    test("fi", "SimpleSpeak", expr, "t arvolla x");
 }
 
 
 #[test]
 fn explicit_times_with_parens() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2062;</mo><mrow><mo>(</mo><mi>x</mi><mo>)</mo></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t times x");
+    test("fi", "SimpleSpeak", expr, "t kertaa x");
 }
 
 #[test]
 fn explicit_function_call() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2061;</mo><mrow><mi>x</mi></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t of x");
+    test("fi", "SimpleSpeak", expr, "t arvolla x");
 }
 
 #[test]
 fn explicit_times() {
     let expr = "<math><mrow><mi>t</mi><mo>&#x2062;</mo><mrow><mi>x</mi></mrow></mrow></math>";
-    test("en", "SimpleSpeak", expr, "t x");
+    test("fi", "SimpleSpeak", expr, "t x");
 }
 
 
@@ -150,7 +145,7 @@ fn explicit_times() {
 #[test]
 fn no_times_binomial() {
     let expr = "<math><mrow><mi>x</mi> <mo>&#x2062;</mo> <mi>y</mi></mrow></math>";
-    test("en", "SimpleSpeak", expr, "x y");
+    test("fi", "SimpleSpeak", expr, "x y");
 }
 
 #[test]
@@ -159,7 +154,7 @@ fn times_following_paren() {
         <mn>2</mn>
         <mrow>  <mo>(</mo> <mn>3</mn>  <mo>)</mo> </mrow>
         </mrow></math>";
-    test("en", "SimpleSpeak", expr, "2 times 3");
+    test("fi", "SimpleSpeak", expr, "2 kertaa 3");
 }
 
 #[test]
@@ -168,7 +163,7 @@ fn times_preceding_paren() {
         <mrow>  <mo>(</mo> <mn>2</mn>  <mo>)</mo> </mrow>
         <mn>3</mn>
         </mrow></math>";
-    test("en", "SimpleSpeak", expr, "2 times 3");
+    test("fi", "SimpleSpeak", expr, "2 kertaa 3");
 }
 
 #[test]
@@ -179,8 +174,8 @@ fn no_times_sqrt() {
         <mo>=</mo>
         <msqrt> <mrow>  <mi>a</mi><mi>b</mi></mrow> </msqrt>
         </mrow></math>";
-    test("en", "SimpleSpeak", expr, 
-            "the square root of eigh; the square root of b; is equal to, the square root of eigh b end root,");
+    test("fi", "SimpleSpeak", expr, 
+            "neliöjuuri a, neliöjuuri b; on yhtä suuri kuin, neliöjuuri a b loppu juuri,");
 }
 
 /*
@@ -194,7 +189,7 @@ fn no_times_sqrt() {
         <mo>)</mo></mrow>
         <mi>x</mi>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "25 times x");
+        test("fi", "SimpleSpeak", expr, "25 kertaa x");
     }
 
     #[test]
@@ -205,7 +200,7 @@ fn no_times_sqrt() {
         <mrow><mi>x</mi><mi>y</mi></mrow>
         <mo>)</mo></mrow>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "b x y");
+        test("fi", "SimpleSpeak", expr, "b x y");
     }
 
     #[test]
@@ -216,7 +211,7 @@ fn no_times_sqrt() {
         <mrow><mo>&#x2212;</mo><mn>2</mn></mrow>
         <mo>)</mo></mrow>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "2 plus negative 2");
+        test("fi", "SimpleSpeak", expr, "2 plus negatiivinen 2");
     }
 
 
@@ -228,7 +223,7 @@ fn no_times_sqrt() {
         <mo>)</mo></mrow>
         <mo>+</mo><mn>1</mn>
         </mrow></math>";
-        test("en", "SimpleSpeak", expr, "negative 2 x, plus 1");
+        test("fi", "SimpleSpeak", expr, "negatiivinen 2 x, plus 1");
     }
 
     #[test]
@@ -244,7 +239,7 @@ fn no_times_sqrt() {
         </msup>
         </mrow>
     </mrow></math>";
-        test("en", "SimpleSpeak", expr, "open paren 2 x close paren squared");
+        test("fi", "SimpleSpeak", expr, "auki sulku 2 x kiinni sulku neliö");
     }
 
     #[test]
@@ -257,7 +252,7 @@ fn no_times_sqrt() {
             <mfrac> <mn>1</mn><mn>2</mn></mfrac>
             <mo>)</mo></mrow></mrow>
     </mrow></math>";
-        test("en", "SimpleSpeak", expr, "2 plus 1 half");
+        test("fi", "SimpleSpeak", expr, "2 plus 1 kahdesosa");
     }
 
 
@@ -269,7 +264,7 @@ fn no_times_sqrt() {
         <mrow> <mo arg='open'>(</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>)</mo>
         <mo>)</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr, "the open interval from c to d");
+    test("fi", "SimpleSpeak",expr, "avoin väli luvusta c lukuun d");
 }
 
 #[test]
@@ -279,7 +274,7 @@ fn no_times_sqrt() {
             <mrow> <mo arg='open'>[(]</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>)</mo>
             <mo>)</mo></mrow>
         </math>";
-    test("en", "SimpleSpeak",expr, "the closed open interval from c to d");
+    test("fi", "SimpleSpeak",expr, "oikealta puoliavoin väli luvusta c lukuun d");
 }
 
 
@@ -290,7 +285,7 @@ fn parens_interval_open_closed() {
         <mrow> <mo arg='open'>(</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>]</mo>
         <mo>]</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr,"the open closed interval from c to d");
+    test("fi", "SimpleSpeak",expr,"vasemmalta puoliavoin väli luvusta c lukuun d");
 }
 
 
@@ -301,7 +296,7 @@ fn parens_interval_closed_closed() {
             <mrow> <mo arg='open'>[(]</mo><mi arg='start'>c</mi><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>]</mo>
             <mo>]</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr, "the closed interval from c to d");
+    test("fi", "SimpleSpeak",expr, "suljettu väli luvusta c lukuun d");
 }
 
     #[test]
@@ -311,8 +306,8 @@ fn parens_interval_closed_closed() {
         <mrow><mrow arg='start'><mo>-</mo> <mi>∞</mi></mrow><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>)</mo>
         <mo>)</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr,
-    "the open interval from negative infinity to d");
+    test("fi", "SimpleSpeak",expr,
+    "avoin väli luvusta negatiivinen ääretön lukuun d");
 }
 
     #[test]
@@ -322,7 +317,7 @@ fn parens_interval_closed_closed() {
         <mrow><mrow arg='start'><mo>-</mo> <mi>∞</mi></mrow><mo>,</mo><mi arg='end'>d</mi></mrow><mo arg='close'>]</mo>
         <mo>]</mo></mrow>
     </math>";
-    test("en", "SimpleSpeak",expr,
-    "the open closed interval from negative infinity to d");
+    test("fi", "SimpleSpeak",expr,
+    "vasemmalta puoliavoin väli luvusta negatiivinen ääretön lukuun d");
 }
 
