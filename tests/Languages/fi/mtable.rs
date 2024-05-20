60836bd19c79e2fbd7e0f0f811a169053d8b3037
samimaat <saminiksi@gmail.com>
2024-05-20 14:42:49 +0300

    translate rest with comment
    This test fails, since mathcat produces "absolute value of cap s" and not "cardinality of cap s" in Finnish. Don't know why

--- tests/Languages/fi/mtable.rs
+++ tests/Languages/fi/mtable.rs
@@ -538,7 +538,7 @@ fn simple_cardinality_value() {
     <mrow><mrow><mo>|</mo> <mi>S</mi> <mo>|</mo></mrow></mrow>
   </math>";
   test_prefs("fi", "ClearSpeak", vec![("Verbosity", "Medium"), ("ClearSpeak_AbsoluteValue", "Cardinality")], expr,
-             "the cardinality of cap s,");
+             "kardinaliteetti s,");
 }
   
 // Test preferences
