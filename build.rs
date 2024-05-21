a3e8397810db521858f9de4c015e5d4810691532
NSoiffer <NSoiffer@users.noreply.github.com>
2024-05-21 05:41:56 +0100

    Zips up each Language and Braille dir individually.
    Changed to using bzip2 for compression as it seems to be about 20% better size-wise.

--- build.rs
+++ build.rs
@@ -10,8 +10,9 @@ use std::path::{Path, PathBuf};
 use zip::write::{ZipWriter, SimpleFileOptions};
 use zip::CompressionMethod;
 
-fn zip_dir(parent_dir: &Path, options: SimpleFileOptions) -> Result<(), std::io::Error> {
-    for entry in read_dir(parent_dir)? {
+fn zip_dir(rules_dir: &Path, sub_dir_name: &str, archive_zip: &mut ZipWriter<File>, options: SimpleFileOptions) -> Result<(), std::io::Error> {
+    let rules_sub_dir = rules_dir.join(sub_dir_name);
+    for entry in read_dir(rules_sub_dir)? {
         let entry = entry?;
         let entry_path = entry.path();
 
@@ -25,25 +26,49 @@ fn zip_dir(parent_dir: &Path, options: SimpleFileOptions) -> Result<(), std::io:
         if entry_path.is_dir(){
             if let Some(dir_name) = entry_path.components().last() {
                 let dir_name = dir_name.as_os_str().to_str().unwrap();
+                if dir_name == "zz" {       // test dir
+                    continue;
+                }
                 if  std::env::set_current_dir(&entry_path).is_err() {
                     println!("cargo::warning=couldn't change to directory '{}'", &entry_path.display());
                     continue;
                 }
                 let zip_name = dir_name.to_string() + ".zip";
                 // println!("zip_name='{}')", &zip_name);
-                let archive_path = PathBuf::from(&zip_name);
+                let zip_path = PathBuf::from(&zip_name);
                 // println!("archive_path: '{}'", archive_path.to_string_lossy());
-                let zip_file = match File::create(archive_path) {
+                let zip_file = match File::create(&zip_path) {
                     Ok(file) => file,
                     Err(e) => panic!("build.rs couldn't create {:?}: {}", &zip_name, e),
                 };
                 let mut zip = ZipWriter::new(zip_file);
                 zip_entry(&mut zip, &PathBuf::from("."), options)?;
                 zip.finish()?;
+
+                // Add the file to full archive with the proper relative path (e.g., Languages/en)
+                std::env::set_current_dir(rules_dir.join(".."))?;
+                let zip_file_path_for_archive = PathBuf::from("Rules").join(sub_dir_name).join(dir_name).join(zip_name);
+                add_file_to_zip(archive_zip, &zip_file_path_for_archive, options)?;
             }
         }
     }
     return Ok( () );
+}
+
+fn add_file_to_zip<W: Write + Seek>(
+    zip: &mut ZipWriter<W>,
+    path: &Path,
+    options: SimpleFileOptions,
+) -> io::Result<()> {
+    zip.start_file_from_path(path, options)?;
+
+    let mut file = File::open(&path)?;
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+    zip.write_all(&buffer)?;
+    // println!("  ..finished '{}'", path.to_str().unwrap());
+    return Ok(());
 }
 
 #[allow(clippy::unused_io_amount)]
@@ -61,19 +86,11 @@ fn zip_entry<W: Write + Seek>(
     } else if let Some(suffix) = path.extension() {
         let suffix = suffix.to_ascii_lowercase();
         if suffix == "yaml" || suffix == "yml" {
-            // println!("  adding '{}'", path.to_str().unwrap());
-            zip.start_file_from_path(path, options)?;
-
-            let mut file = File::open(&path)?;
-            let mut buffer = Vec::new();
-    
-            file.read_to_end(&mut buffer)?;
-            zip.write_all(&buffer)?;
-            // println!("  ..finished '{}'", path.to_str().unwrap());
+            add_file_to_zip(zip, path, options)?;
         }
     }
 
-    Ok(())
+    return Ok(());
 }
 
 /// Zip up each language and braille dir
@@ -82,24 +99,31 @@ fn main() {
     // This doesn't work because the build claims OUT_DIR is not defined(?)
     // let archive = PathBuf::from(concat!(env!("OUT_DIR"),"/rules.zip"));
 
-    // let out_dir = std::env::var_os("OUT_DIR").unwrap();
-    // let archive: PathBuf = [out_dir, std::ffi::OsString::from("rules.zip")].iter().collect();
-    // eprintln!("zip file location: '{:?}'", archive.to_str());
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let archive_path = PathBuf::from(out_dir).join("rules.zip");     // A zip file containing all the zip files.
+    println!("zip file location: '{:?}'", archive_path.to_str());
 
-    let zip_options = SimpleFileOptions::default().compression_method(CompressionMethod::Deflated)
+    let zip_options = SimpleFileOptions::default().compression_method(CompressionMethod::BZIP2)
                     .compression_level(Some(9));
 
-    let rules_dir = match std::env::current_exe().unwrap().parent().unwrap()
-                            .join("../../../../Rules/")
-                            .canonicalize() {
-                        Ok(path) => path,
-                        Err(err) => panic!("Error canonicalizing path: {}", err),
-                    };
-    if let Err(e) = zip_dir(&rules_dir.join("Languages"), zip_options.clone()) {
+    let rules_dir = std::env::current_dir().unwrap().join("Rules") ;
+    println!("rules directory '{:?}'", &rules_dir.to_string_lossy());
+    let archive_zip_file = match File::create(&archive_path) {
+        Ok(file) => file,
+        Err(e) => panic!("build.rs couldn't create {:?}: {}", &archive_path.to_str(), e),
+    };
+
+    let mut archive_zip = ZipWriter::new(archive_zip_file);
+    
+    if let Err(e) = zip_dir(&rules_dir, "Languages", &mut archive_zip, zip_options.clone()) {
         panic!("Error: {}", e);
     }
-    if let Err(e) = zip_dir(&rules_dir.join("Braille"), zip_options) {
+    if let Err(e) = zip_dir(&rules_dir, "Braille", &mut archive_zip, zip_options) {
         panic!("Error: {}", e);
     }
+    if let Err(e) = archive_zip.finish() {
+        panic!("Error in zip.finish(): {}", e);
+    }
+
     println!("cargo::rerun-if-changed=Rules");
 }
