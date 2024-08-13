ff251b26f3d0ef82e9db8beb6ff9537f6d861aae
NSoiffer <soiffer@alum.mit.edu>
2024-08-13 01:00:31 +0100

    Fix up some table examples to be get alignment as it would normally be done. This also adds some empty elements which is important for testing.

--- tests/Languages/en/ClearSpeak/multiline.rs
+++ tests/Languages/en/ClearSpeak/multiline.rs
@@ -2,38 +2,30 @@ use crate::common::*;
 
 #[test]
 fn case_1() {
-    // init_logger();
-    let expr = "<math>
-            <mrow>
-            <mi>f</mi><mrow><mo>(</mo>
-            <mi>x</mi>
-            <mo>)</mo></mrow><mo>=</mo><mrow><mo>{</mo> <mrow>
-            <mtable>
-            <mtr>
-                <mtd>
-                <mrow>
-                <mo>&#x2212;</mo><mn>1</mn><mtext>&#x00A0;if&#x00A0;</mtext><mi>x</mi><mo>&#x003C;</mo><mn>0</mn></mrow>
-                </mtd>
-            </mtr>
-            <mtr>
-                <mtd>
-                <mrow>
-                <mn>0</mn><mtext>&#x00A0;if&#x00A0;</mtext><mi>x</mi><mo>=</mo><mn>0</mn></mrow>
-                </mtd>
-            </mtr>
-            <mtr>
-                <mtd>
-                <mrow>
-                <mn>1</mn><mtext>&#x00A0;if&#x00A0;</mtext><mi>x</mi><mo>&#x003E;</mo><mn>0</mn></mrow>
-                </mtd>
-            </mtr>
-            </mtable></mrow> </mrow></mrow>
-        </math>
+  let expr = "<math>
+    <mi>f</mi>
+    <mrow>
+      <mo>(</mo>
+      <mi>x</mi>
+      <mo>)</mo>
+    </mrow>
+    <mo>=</mo>
+    <mrow>
+      <mo stretchy='true'>{</mo>
+      <mtable>
+        <mtr><mtd><mo>-</mo><mn>1</mn></mtd><mtd><mtext>if</mtext></mtd><mtd><mi>x</mi><mo>&lt;</mo><mn>0</mn></mtd></mtr>
+        <mtr><mtd><mn>0</mn></mtd><mtd><mtext>if</mtext></mtd><mtd><mi>x</mi><mo>=</mo><mn>0</mn></mtd></mtr>
+        <mtr><mtd><mn>1</mn></mtd><mtd><mtext>if</mtext></mtd><mtd><mi>x</mi><mo>&gt;</mo><mn>0</mn></mtd></mtr>
+      </mtable>
+    </mrow>
+  </math>
    ";
-   test_ClearSpeak("en", "ClearSpeak_MultiLineLabel", "Auto", expr, "f of x is equal to; 3 cases, \
-                case 1; negative 1 if x; is less than 0; \
-                case 2; 0 if x, is equal to 0; \
-                case 3; 1 if x, is greater than 0;");
+   test_ClearSpeak("en", "ClearSpeak_MultiLineLabel", "Auto", expr,
+    "f of x is equal to; 3 cases, \
+                case 1; negative 1 if x is less than 0; \
+                case 2; 0 if x is equal to 0; \
+                case 3; 1 if x is greater than 0;"
+    )
 }
 
 #[test]
@@ -43,6 +35,23 @@ fn equation_auto() {
       <mtable>
        <mtr> <mtd> <mrow> <mi>x</mi><mo>+</mo><mi>y</mi></mrow> </mtd>  <mtd><mo>=</mo> </mtd>  <mtd><mn>7</mn></mtd>  </mtr>
        <mtr> <mtd> <mrow> <mn>2</mn><mi>x</mi><mo>+</mo><mn>3</mn><mi>y</mi></mrow></mtd>  <mtd><mo>=</mo></mtd>  <mtd><mrow><mn>17</mn></mrow></mtd> </mtr>
+      </mtable></mrow>
+    </math>
+   ";
+   test_ClearSpeak("en", "ClearSpeak_MultiLineLabel", "Auto", expr,
+                "2 lines, \
+                line 1; x plus y, is equal to, 7; \
+                line 2; 2 x plus 3 y, is equal to, 17;");
+}
+
+
+#[test]
+fn equation_plus_at_start() {
+  let expr = "<math>
+     <mrow>
+      <mtable>
+       <mtr> <mtd> <mi>x</mi></mtd><mtd><mo>+</mo><mi>y</mi> </mtd>  <mtd><mo>=</mo> </mtd>  <mtd><mn>7</mn></mtd>  </mtr>
+       <mtr> <mtd> <mn>2</mn><mi>x</mi></mtd><mtd><mo>+</mo><mn>3</mn><mi>y</mi></mtd>  <mtd><mo>=</mo></mtd>  <mtd><mrow><mn>17</mn></mrow></mtd> </mtr>
       </mtable></mrow>
     </math>
    ";
@@ -61,9 +70,8 @@ fn equation_case() {
       </mtable></mrow>
     </math>
    ";
-   test_ClearSpeak("en", "ClearSpeak_MultiLineLabel", "Case", expr, "2 cases, \
-                case 1; x plus y, is equal to, 7; \
-                case 2; 2 x plus 3 y, is equal to, 17;");
+   test_ClearSpeak("en", "ClearSpeak_MultiLineLabel", "Case", expr, 
+   "2 cases, case 1; x plus y, is equal to, 7; case 2; 2 x plus 3 y, is equal to, 17;");
 }
 
 #[test]
@@ -154,4 +162,17 @@ fn equation_step() {
    test_ClearSpeak("en", "ClearSpeak_MultiLineLabel", "Step", expr, "2 steps, \
                 step 1; x plus y, is equal to, 7; \
                 step 2; 2 x plus 3 y, is equal to, 17;");
+}
+
+#[test]
+fn continued_row() {
+  let expr = "<math>
+  <mtable intent=':system-of-equations'>
+   <mtr><mtd><mi>x</mi></mtd><mtd><mo>=</mo></mtd><mtd><mi>y</mi></mtd></mtr>
+   <mtr intent=':continued-row'><mtd/><mtd/><mtd><mrow><mo>+</mo><mn>1</mn></mrow></mtd></mtr>
+   <mtr><mtd><mi>y</mi></mtd><mtd><mo>=</mo></mtd><mtd><mn>1</mn></mtd></mtr>
+  </mtable>
+</math>";
+test("en", "SimpleSpeak", expr,
+     "2 equations, equation 1; x, is equal to, y plus 1; equation 2; y, is equal to, 1;");
 }
