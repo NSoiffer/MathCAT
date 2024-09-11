b3ccb1a611ea00b532caf30a3ea2feab79213e22
AndersEkl <anders.eklund@spsm.se>
2024-09-11 14:40:06 +0200

    Removed template comment

--- tests/braille/Swedish/swedish.rs
+++ tests/braille/Swedish/swedish.rs
@@ -3,13 +3,6 @@
 //  https://www.mtm.se/globalassets/punktskriftsnamnden/punktskriftens_skrivregler_matematik.pdf
 use crate::common::*;
 
-// #[test]
-// fn ex_1_1 {
-//     let expr = "<math><mn>6</mn><mo>=</mo><mn>1</mn><mo>&#xD7;</mo><mn>2</mn><mo>&#xD7;</mo><mn>3</mn>
-//                 <mo>=</mo><mn>1</mn><mo>+</mo><mn>2</mn><mo>+</mo><mn>3</mn></math>";
-//     test_braille("Swedish", expr, "⠼⠋⠀⠐⠶⠀⠼⠁⠐⠦⠼⠃⠐⠦⠼⠉⠀⠐⠶⠀⠼⠁⠐⠖⠼⠃⠐⠖⠼⠉");
-// }
-
 // CHAPTER 5
 
 #[test]
