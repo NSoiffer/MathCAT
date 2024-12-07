16196ef968b1d13dda2090f49b2e6b2a88f4be12
NSoiffer <NSoiffer@users.noreply.github.com>
2024-12-06 16:36:08 -0800

    add pause for comma

--- tests/Languages/en/SimpleSpeak/sets.rs
+++ tests/Languages/en/SimpleSpeak/sets.rs
@@ -162,7 +162,7 @@ fn multiple_element_set() {
     let expr = "<math>
                 <mo>{</mo> <mn>5</mn> <mo>,</mo> <mn>10</mn>  <mo>,</mo> <mn>15</mn> <mo>}</mo>
             </math>";
-    test("en", "SimpleSpeak", expr, "the set 5 comma 10 comma 15");
+    test("en", "SimpleSpeak", expr, "the set 5 comma, 10 comma, 15");
 }
 
 #[test]
