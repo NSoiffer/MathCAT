23e9477bb4c508aa9f838b44ce5a60d6c50d68fd
NSoiffer <NSoiffer@users.noreply.github.com>
2025-08-18 10:18:27 -0700

    Fix https://github.com/NSoiffer/MathCATForPython/issues/93. The fallback to English failed to change the failed language for the name of the zip file to "en" when handling the fallback to "en".

--- src/prefs.rs
+++ src/prefs.rs
@@ -460,10 +460,11 @@ impl PreferenceManager {
         
         // ignore regional subdirs
         let dir = PreferenceManager::get_language_dir(path, language, default_lang)?;
+        let language = if dir.ends_with(language) {language} else {dir.file_name().unwrap().to_str().unwrap()};
         let zip_file_name = language.to_string() + ".zip";
         let zip_file_path = dir.join(&zip_file_name);
         let zip_file_string = zip_file_path.to_string_lossy().to_string();
-        // debug!("unzip_files: dir: {}, zip_file_name: {}, zip_file_path: {}", dir.to_string_lossy(), zip_file_name, zip_file_string);
+        debug!("unzip_files: dir: {}, zip_file_name: {}, zip_file_path: {}", dir.to_string_lossy(), zip_file_name, zip_file_string);
         if UNZIPPED_FILES.with( |unzipped_files| unzipped_files.borrow().contains(&zip_file_string)) {
             return Ok(false);
         }
